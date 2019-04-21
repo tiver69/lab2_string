5ae16b29020a811c9a2dbe3a8338095a5eed50ac
Aleksandra <kibko.sasha@gmail.com>
2019-04-21 21:26:01 +0300

    small modification to main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,7 @@ import model.StringTaskComparator;
 import model.text.Text;
 import service.*;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
 
@@ -14,23 +12,25 @@ public class Main {
         Text convertedText = TextParser.parseText(inputText);
         System.out.println(inputText);
 
-        Set<String> wordsFromInput = RegularExpressionFilter.firstVowelText(inputText);
-        System.out.println("Input before sorting:\n" + wordsFromInput);
+        long time = System.currentTimeMillis();
+            List<String> wordsFromInput = new ArrayList<>(RegularExpressionFilter.firstVowelText(inputText));
+            System.out.println("Input before sorting:\n" + wordsFromInput);
+            wordsFromInput.sort(new StringTaskComparator());
+            System.out.println("After sorting:\n" +  wordsFromInput + "");
+        System.out.println("Time with string variable: " + (System.currentTimeMillis() - time) + "\n");
 
-        Set<String> wordsFromConverted = RegularExpressionFilter.firstVowelText(convertedText);
-        System.out.println("Converted before sorting:\n" + wordsFromConverted);
-        System.out.println("Same? : "+ wordsFromConverted.equals(wordsFromConverted));
+        time = System.currentTimeMillis();
+            List<String> wordsFromConverted = new ArrayList<>(RegularExpressionFilter.firstVowelText(convertedText));
+            System.out.println("Converted before sorting:\n" + wordsFromConverted);
+            wordsFromConverted.sort(new StringTaskComparator());
+            System.out.println("After sorting:\n" +  wordsFromConverted);
+        System.out.println("Time with Text variable: " + (System.currentTimeMillis() - time));
+        System.out.println("Same? : "+ wordsFromInput.equals(wordsFromConverted));
 
-        Set<String> test = new TreeSet<>(new StringTaskComparator());
-        test.addAll(RegularExpressionFilter.firstVowelText(inputText));
-        System.out.println("\nAfter sorting:\n" +  test);
-
-        Set<String> test2 = new TreeSet<>();
+        List<String> test3 = new ArrayList<>();
         for (String word: wordsFromInput) {
-            test2.add(RegularExpressionFilter.firstConsonantWord(word));
+            test3.add(RegularExpressionFilter.firstConsonantWord(word));
         }
-        System.out.println("Sorted by:\n" +  test2);
-
-        System.out.println("Everything was sorted? : "+ wordsFromInput.equals(test));
+        System.out.println("\nSorted by:\n" +  test3);
     }
 }
--- src/main/java/model/StringTaskComparator.java
+++ src/main/java/model/StringTaskComparator.java
@@ -19,10 +19,11 @@ public class StringTaskComparator implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         if (o1 == null || o2 ==null) throw new NullPointerException("Word is empty");
+        if (o1.equals(o2)) return 0;
         String fromConsonantO1 = RegularExpressionFilter.firstConsonantWord(o1);
         String fromConsonantO2 = RegularExpressionFilter.firstConsonantWord(o2);
         int compareResult = fromConsonantO1.compareTo(fromConsonantO2);
-        if (compareResult==0) return -1;
+        if (compareResult == 0) return -1;
         else return compareResult;
     }
 }
